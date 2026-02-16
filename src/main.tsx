ca92af13dc0fd5a80f4c13a4859b0559c8e60dd5
Fuji-Noobie-Ghost <fitahiana.ratia@gmail.com>
2026-02-16 22:40:01 +0300

    feat(ui): add MUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '@fontsource/poppins/300.css'; // Light weight
+import '@fontsource/poppins/400.css'; // Regular weight
+import '@fontsource/poppins/500.css'; // Medium weight
+import '@fontsource/poppins/700.css'; // Bold weight
+import { App } from './App.tsx'
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Poppins", sans-serif',
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
